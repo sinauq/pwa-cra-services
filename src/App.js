82ec3bf4ce174ff9b5ba390f7820676e70f3a60d
unknown <kiana.zrb@gmail.com>
2021-11-16 09:43:35 +0330

    added cache message

--- src/App.js
+++ src/App.js
@@ -4,14 +4,9 @@ import Form from "./components/Form";
 import ConnectionCheck from "./components/ConnectionCheck";
 const sampleApi = "https://reqres.in/api/users";
 
-async function addCache(urls) {
-  const myCache = await window.caches.open("response-cache");
-  await myCache.addAll(urls);
-  console.log("added cache", myCache);
-}
-
 function App({ broadcast }) {
   const [users, setUsers] = useState([]);
+  const [message, setMessage] = useState("")
 
   useEffect(() => {
     fetch(sampleApi)
@@ -19,12 +14,20 @@ function App({ broadcast }) {
       .then(res => setUsers(res));
   }, []);
 
+  async function addCache(urls) {
+    const myCache = await window.caches.open("response-cache");
+    await myCache.addAll(urls);
+    console.log("added cache", myCache);
+    setMessage("added cache")
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <ConnectionCheck />
         <p>{users && JSON.stringify(users)}</p>
         <button onClick={() => addCache([sampleApi])}>Register Router</button>
+        <p style={{ color: "red", fontSize: "14px" }}>{message}</p>
         <Form sampleApi={sampleApi} />
       </header>
     </div>
